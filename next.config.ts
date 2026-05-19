0c1845e4aedeeb9814f69f6984c1e3296e1973b7
Aser Alemu <asertesfaye@hotmail.com>
2026-05-19 22:13:06 +0300

    package update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   basePath: "/cortext_ai_agent_website",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;

ede3880dfd18fcf7837927e33ce8e58cb5c07abd
Aser Alemu <asertesfaye@hotmail.com>
2026-05-20 10:19:17 +0300

    Updates

--- _next/static/A_kds317lC9AtZ05Ybliz/_buildManifest.js
+++ _next/static/A_kds317lC9AtZ05Ybliz/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/cortext-ai-agent-website//_next/:path+",
+        "destination": "/cortext-ai-agent-website/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
